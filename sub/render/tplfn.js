925518a9286ed862f75d8d64dfd77d1e0c6e04fd
yc <1067835630@qq.com>
2020-11-25 16:22:47 +0800

    fix:修改imgsrc

--- sub/render/tplfn.js
+++ sub/render/tplfn.js
@@ -10,13 +10,13 @@ export default function anonymous(it) {
       item = arr1[index += 1];
       out += ' ';
       // if (index) {
-        out += ' <image class="frbox" src="img/WeChatRank_FriendPanel.png"><view class="listItem listItemOld">  ';
+        out += ' <image class="frbox" src="sub/img/WeChatRank_FriendPanel.png"><view class="listItem listItemOld">  ';
       // }
       // out += ' ';
       // if (index % 2 === 0) {
       //   out += ' <view class="listItem"><image class="frbox" src="img/WeChatRank_FriendPanel.png"> </image>';
       // }
-      out += ' <text class="listItemNum" value="' + (index + 1) + '"></text> <image class="listHeadImg" src="' + (item.avatarUrl) + '"></image> <view><text class="listItemName" value="' + (item.nickname) + '"></text><text class="numbertop" value="' + (item.score) + '"></text></view><image class="gift" src="img/WeChatRank_GiftButton.png"></image></view></image> ';
+      out += ' <text class="listItemNum" value="' + (index + 1) + '"></text> <image class="listHeadImg" src="' + (item.avatarUrl) + '"></image> <view><text class="listItemName" value="' + (item.nickname) + '"></text><text class="numbertop" value="' + (item.score) + '"></text></view><image class="gift" src="sub/img/WeChatRank_GiftButton.png"></image></view></image> ';
     }
   }
   out += ' </scrollview> <view class="listItem selfListItem"> <text class="listItemNum" value="' + (it.selfIndex) + '"></text> <image class="listHeadImg" src="' + (it.self.avatarUrl) + '"></image> <view><text class="listItemName" value="' + (it.self.nickname) + '"></text><text class="numbertop" value="' + (it.self.score) + '"></text></view></view></view> </view></view></view>';
